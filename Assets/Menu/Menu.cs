c60e9d2066d31eee0b44321454c2113b6c31ed24
Lucaslyx2001 <lemonlyx@gmail.com>
2023-01-04 15:15:59 +0800

    menu update
    fk

---
+++ Assets/Menu/Menu.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public GameObject creditPanel, settingPanel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ShowCredit()
+    {
+        creditPanel.SetActive(true);
+    }
+
+    public void ShowSetting()
+    {
+        settingPanel.SetActive(true);
+    }
+
+    public void StartPlay()
+    {
+        SceneManager.LoadScene("Low Poly Road Pack Demo");
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
