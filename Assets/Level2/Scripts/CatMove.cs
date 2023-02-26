376c8b353d76a99f197fa3c1c5d57e990ba52c7c
Lucaslyx2001 <lemonlyx@gmail.com>
2023-02-26 19:35:07 +0800

    dmlv2

---
+++ Assets/Level2/Scripts/CatMove.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class CatMove : MonoBehaviour
+{
+    private UnityEngine.AI.NavMeshAgent _agent;
+
+    public GameObject Player;
+
+    public float EnemyDistanceRun = 4.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance = Vector3.Distance(transform.position, Player.transform.position);
+
+        Debug.Log("Distance: " + distance);
+
+        if(distance < EnemyDistanceRun){
+            Vector3 dirToPlayer = transform.position - Player.transform.position;
+
+            Vector3 newPos = transform.position + dirToPlayer;
+
+            _agent.SetDestination(newPos);
+        }
+    }
+}
